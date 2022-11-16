ad86a9bbeee94a5992ecfa9851735c0f1da564df
Ismael Mousa <ismaelramzimousa@gmail.com>
2022-11-16 18:10:22 +0200

    Men Pants Page Added

---
+++ src/components/MenPantsPage.js
@@ -0,0 +1,13 @@
+import MenPantsCard from './MenPantsCard'
+
+const MenPantsPage =()=> {
+    return (
+    <section><MenPantsCard></MenPantsCard>
+    <MenPantsCard></MenPantsCard>
+    <MenPantsCard></MenPantsCard>
+    <MenPantsCard></MenPantsCard>
+    <MenPantsCard></MenPantsCard>
+    </section>
+    );
+}
+export default MenPantsPage;
