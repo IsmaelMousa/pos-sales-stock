554e6cc5892efebdba9ad18ec9abea892d91e87d
batool <s12115673@stu.najah.edu>
2022-11-26 01:30:44 +0200

    Women Subcategory cards and page added

--- src/components/WomenSubCategoryCards.js
+++ src/components/WomenSubCategoryCards.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import'./WomenSubCategory.css';
+import'./WomenSubCategoryCards.css';
 
-const WomenSubCategory= (props) =>{
+const WomenSubCategoryCards= (props) =>{
     return (
         <div className="card text-center "> 
             <div className="overflow">
-                <img src={props.imgsrc} alt="image" className='card-img-top'/>
+                <img src={props.imgsrc} alt="..." className='card-img-top'/>
             </div>
 <div className='card-body text-dark '>
 <h4 className='card-title'> {props.title}</h4>
-<a href='#' className='btn btn-outline'> GO </a>
+<a href='#' className='btn btn-outline'><b> GO </b> </a>
 
 
 
@@ -18,4 +18,4 @@ const WomenSubCategory= (props) =>{
     
 }
 
-export default WomenSubCategory;
+export default WomenSubCategoryCards;
