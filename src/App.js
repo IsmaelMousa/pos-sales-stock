4839eeb7c38d4d0bb308ce35bee3a42ecd02adf4
Ismael Mousa <ismaelramzimousa@gmail.com>
2022-11-26 15:39:07 +0200

    handle app.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,5 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-function App()
-{
-  return (<div></div>);  
-}
-=======
->>>>>>> 2774b82eb37f13f68d5f2964d4532eba95ffed8b
-=======
->>>>>>> 0af05f2f4915b7843a716a6e2e96970024d00d69
 
 function App() {
-  return (<div> </div>
-  );
+  return (<div> </div>);
 }
 export default App;
