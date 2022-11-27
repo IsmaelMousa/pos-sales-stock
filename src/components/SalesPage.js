40253076954814ea3f342d568876894c31ef0d54
Ismael Mousa <ismaelramzimousa@gmail.com>
2022-11-27 22:50:47 +0200

    Sales page and cards , not finished

--- src/components/SalesPage.js
+++ src/components/SalesPage.js
@@ -1,64 +1,49 @@
-import SalesItemCard from './SalesItemCard';
-import './SalesItemCard.css'
-import image1 from '../images/pexels-el-sultan-3220386.jpg'
-import image2 from '../images/pexels-lloyd-james-12113101.jpg'
-import image3 from '../images/pexels-shvets-production-6974955.jpg'
-import image4 from '../images/pexels-ahmed-samir-12024071.jpg'
-import image5 from '../images/pexels-monstera-6311142.jpg'
-import image6 from '../images/pexels-monstera-6311150.jpg'
-import image7 from '../images/pexels-monstera-6311588.jpg'
-import image8 from '../images/pexels-monstera-6311629.jpg'
-import image9 from '../images/pexels-monstera-6311647.jpg'
-import image10 from '../images/pexels-mart-production-9558243.jpg'
-import image11 from '../images/pexels-evg-kowalievska-3993234.jpg'
-import image12 from '../images/pexels-raul-reynoso-1018911.jpg'
-
-
-const SalesPage = () => {
-  return (
-    <div className="container-fluid d-flex justify-content-center">
-      <div className='row '>
-        <div className='col-md-3 pt-3'>
-          <SalesItemCard Name="Tshirt " prePrice="1200" newPrice="6000" remainingQuantity="150" imageSrc={image1} />
-        </div>
-        <div className='col-md-3 pt-3'>
-          <SalesItemCard Name="Oversize Shirt" prePrice="800" newPrice="400" remainingQuantity="15" imageSrc={image2} />
-        </div>
-        <div className='col-md-3 pt-3'>
-          <SalesItemCard Name="Hoodie" prePrice="550" newPrice="300" remainingQuantity="66" imageSrc={image3} />
-        </div>
-        <div className='col-md-3 pt-3'>
-          <SalesItemCard Name="Shirt" prePrice="200" newPrice="75" remainingQuantity="143" imageSrc={image4} />
-        </div>
-        <div className='col-md-3 pt-3'>
-          <SalesItemCard Name="Hoodie" prePrice="650" newPrice="400" remainingQuantity="80" imageSrc={image5} />
-        </div>
-        <div className='col-md-3 pt-3'>
-          <SalesItemCard Name="Hoodie" prePrice="750" newPrice="460" remainingQuantity="53" imageSrc={image6} />
-        </div>
-
-        <div className='col-md-3 pt-3'>
-          <SalesItemCard Name="Tshirt" prePrice="330" newPrice="300" remainingQuantity="79" imageSrc={image7} />
-        </div>
-        <div className='col-md-3 pt-3'>
-          <SalesItemCard Name="Hoodie" prePrice="1000" newPrice="879" remainingQuantity="99" imageSrc={image8} />
-        </div>
-        <div className='col-md-3 pt-3 '>
-          <SalesItemCard Name="Hoodie" prePrice="500" newPrice="399" remainingQuantity="69" imageSrc={image9} />
-        </div>
-
-        <div className='col-md-3 pt-3'>
-          <SalesItemCard Name="Oversize Shirt" prePrice="785" newPrice="574" remainingQuantity="11" imageSrc={image10} />
-        </div>
-        <div className='col-md-3 pt-3'>
-          <SalesItemCard Name="shirt" prePrice="508" newPrice="354" remainingQuantity="3" imageSrc={image11} />
-        </div>
-        <div className='col-md-3 pt-3'>
-          <SalesItemCard Name="Tshirt" prePrice="324" newPrice="143" remainingQuantity="85" imageSrc={image12} />
-
-        </div>
-      </div>
+import SalesCard from "./SalesCard";
+import img1 from'../images/Black-Sweat-Pants.jpg';
+import img2 from"../images/Cheino-brown-pants.webp";
+import img3 from"../images/Dark-Cargo-pants.jpg";
+import img4 from"../images/Gray-formal-pants.jpg";
+import img5 from"../images/Jeans-1.webp";
+import img6 from"../images/Off-White-Pants.jpg";
+import img7 from"../images/White-Pants.webp";
+import img8 from"../images/Green-Sweat.jpg";
+import img9 from"../images/Off-White-Pants.jpg";
+import img10 from"../images/White-Pants.webp";
+import img11 from"../images/Green-Sweat.jpg";
+import img12 from"../images/Off-White-Pants.jpg";
+import img13 from"../images/White-Pants.webp";
+import img14 from"../images/Green-Sweat.jpg";
+import img15 from"../images/Green-Sweat.jpg";
+const SalesPage= () =>{
+const data = 
+[
+    { title: "Black Sweat", img:  img1, newPrice: "50$", prePrice:"70$",quantity :13},
+    { title: "Beige Cheino", img:  img2, newPrice: "50$", prePrice:"70$",quantity :21},
+    { title: "Dark Cargo" , img:  img3 , newPrice: "50$", prePrice:"70$",quantity :22},
+    { title: "Gray Formal", img:  img4 , newPrice: "50$", prePrice:"70$",quantity :10},
+    { title: "Blue Jeans", img:  img5 , newPrice: "50$", prePrice:"70$",quantity :66},
+    { title: "Off-White Cheino", img:  img6 , newPrice: "50$", prePrice:"70$",quantity :31},
+    { title: "White Jeans", img:  img7 , newPrice: "50$", prePrice:"70$",quantity :21},
+    { title: "Green Sweat", img:  img8 , newPrice: "50$", prePrice:"70$",quantity :42},
+    { title: "Off-White Cheino", img:  img9 , newPrice: "50$", prePrice:"70$",quantity :31},
+    { title: "White Jeans", img:  img10 , newPrice: "50$", prePrice:"70$",quantity :21},
+    { title: "Green Sweat", img:  img11 , newPrice: "50$", prePrice:"70$",quantity :42},
+    { title: "Off-White Cheino", img:  img12 , newPrice: "50$", prePrice:"70$",quantity :31},
+    { title: "White Jeans", img:  img13 , newPrice: "50$", prePrice:"70$",quantity :21},
+    { title: "Green Sweat", img:  img14 , newPrice: "50$", prePrice:"70$",quantity :42},
+    { title: "Green Sweat", img:  img15 , newPrice: "50$", prePrice:"70$",quantity :42},
+]
+return (
+    <div>
+    {
+        data.map((props) => {
+        return (
+        <SalesCard title={props.title} imgsrc={props.img} newPrice={props.newPrice} quantity={props.quantity} prePrice={props.prePrice}/>
+        )
+    }
+    )
+    }
     </div>
-  );
+);
 }
 export default SalesPage;
