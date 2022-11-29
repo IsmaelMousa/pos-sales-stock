ad48f100a4e264480d0d3009156bc0cb6f710318
Ismael Mousa <ismaelramzimousa@gmail.com>
2022-11-30 00:31:21 +0200

    modify sales card structure

--- src/components/sales-items/SalesCard.js
+++ src/components/sales-items/SalesCard.js
@@ -8,9 +8,7 @@ const SalesCard= (props) =>{
             </div>
 <div className='card-body text-dark '>
 <h4 className='card-title'> {props.title}</h4>
-    <div className='row'>
     <h5  className="card-text">{props.newPrice}<del>{props.prePrice}</del></h5>
-    </div>
 <h6 className='h6'>{props.quantity}</h6>
 <a href='#' className='btn btn-outline'><b> Pick Up </b> </a>
 </div>            
