ba7b447be1841535f6dfb4ed58c64a30bb849781
Ismael Mousa <ismaelramzimousa@gmail.com>
2022-11-30 23:25:35 +0200

    sales items page

--- src/components/sales-items/SalesCard.js
+++ src/components/sales-items/SalesCard.js
@@ -2,17 +2,17 @@ import'./SalesCard.css';
 
 const SalesCard= (props) =>{
     return (
-        <div className="card text-center "> 
-            <div className="overflow">
-                <img src={props.imgsrc} alt="..." className='card-img-top'/>
-            </div>
-<div className='card-body text-dark '>
-<h4 className='card-title'> {props.title}</h4>
-    <h5  className="card-text">{props.newPrice}<del>{props.prePrice}</del></h5>
-<h6 className='h6'>{props.quantity}</h6>
-<a href='#' className='btn btn-outline'><b> Pick Up </b> </a>
-</div>            
-</div>);    
+    <div className="card text-center ">
+        <div className="overflow">
+            <img src={props.imgsrc} alt="..." className='card-img-top'/>
+        </div>
+        <div className='card-body text-dark '>
+            <h4 className='card-title'> {props.title}</h4>
+            <h5  className="card-text">{props.newPrice}<del>{props.prePrice}</del></h5>
+            <h6 className='h6'>{props.quantity}</h6>
+            <a href='#' className='btn btn-outline'><b>Pick Up</b></a>
+        </div>            
+        </div>);    
 }
 
 export default SalesCard;
