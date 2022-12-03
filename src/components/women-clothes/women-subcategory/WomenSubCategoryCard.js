e78883a17842c60fb21173c722f7ed7299f9b7c1
batool <s12115673@stu.najah.edu>
2022-12-04 00:21:49 +0200

    Re-uploud the amendment

--- src/components/women-clothes/women-subcategory/WomenSubCategoryCard.js
+++ src/components/women-clothes/women-subcategory/WomenSubCategoryCard.js
@@ -1,37 +1,21 @@
-import WomenSubCategoryCards from "./WomenSubCategoryCards";
-import img1 from"../images/scarf.png";
-import img2 from"../images/hodie.png";
-import img3 from"../images/jacket.png";
-import img4 from"../images/t-shirt.png";
-import img5 from"../images/pant.png";
-import img6 from"../images/short.png";
-import img7 from"../images/dress.png";
-import img8 from"../images/p.png";
+import React from 'react';
+import'./WomenSubCategoryCards.css';
 
-const WomenSubcategoryCard= () =>{
+const WomenSubCategoryCard= (props) =>{
+    return (
+        <div className="card text-center "> 
+            <div className="overflow">
+                <img src={props.imgsrc} alt="..." className='card-img-top'/>
+            </div>
+<div className='card-body text-dark '>
+<h4 className='card-title'> {props.title}</h4>
+<a href='#' className='btn btn-outline'><b> GO </b> </a>
 
-const data = [
-  { title: "Scarf", img:  img1  },
-  { title: "T-Shirts", img:  img4},
-  { title: "Jacket", img:  img3 },
-  { title: "Pants ", img:  img5 },
-  { title: "Hoodie", img:  img2 },
-  {  title: "Shorts", img:  img6 },
-  {  title: "Dresses", img:  img7 },
-  {  title: "Pajamas", img:  img8 },
-]
-  return (
-    <div>
-      {
-      data.map((props) => {
-        return (
-          <WomenSubCategoryCards title={props.title} imgsrc={props.img}/>
-          )
-      }
-      )
-    }
-  </div>
-  );
+
+
+    </div>            
+        </div>);
+    
 }
 
-export default WomenSubcategoryCard;
+export default WomenSubCategoryCard;
