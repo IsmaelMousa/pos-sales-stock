ed4e1afe9920ecc3268f670fa42775d030c51d1c
unknown <renadiwidat2003@gmail.com>
2022-12-09 02:57:30 +0300

    editing MenSubCategory

--- src/components/men-clothes/men-subcategory/MenSubCategoryCards.js
+++ src/components/men-clothes/men-subcategory/MenSubCategoryCards.js
@@ -1,44 +1,44 @@
-import MenSubCategoryCard from '../components/MenSubCategotyCard';
-import img1 from'../images/scarf.png';
-import img2 from'../images/Coat.png';
-import img3 from'../images/jacket.png';
-import img4 from'../images/t-shirt.png';
-import img5 from'../images/pant.png';
-import img6 from'../images/short.png';
-import img7 from'../images/Sweater.png';
-import img8 from'../images/Shirt.jpg';
+import MenSubCategoryCard from "../men-subcategory/MenSubCategoryCard";
+import img1 from "../../../images/coat-men-1.webp";
+import img2 from "../../../images/jacket-men-sub.webp";
+import img3 from "../../../images/t-shirt-men-sub.webp";
+import img4 from "../../../images/coat-men-1.webp";
+import img5 from "../../../images/coat-men-1.webp";
+import img6 from '../../../images/coat-men-1.webp';
+import img7 from '../../../images/coat-men-1.webp';
+import img8 from '../../../images/coat-men-1.webp';
 
 
-const MenSubCategoryCards=()=>{
-const data = [
+const MenSubCategoryCards = () => {
+  const data = [
 
-   
-{title:"Coat",img: img2},
-{title:"Jacket",img: img3},
-{title:"T-Shirt",img: img4},
-{title:"Pant",img: img5},
-{title:"Sweater",img: img7},
-{title:"Shirt",img: img8},
-{title:"Scarf",img: img1},
-{title:"Short",img: img6},
 
-]
-return (
-   
-    
-<div>
-  {
-    data.map((props)=>{
-      return(
-        <MenSubCategoryCard title={props.title} imgsrc={props.img}/>
-          
-      )
-    }
-    )
-}
- 
-   </div>
+    { title: "Coat", img: img2 },
+    { title: "Jacket", img: img3 },
+    { title: "T-Shirt", img: img4 },
+    { title: "Pant", img: img5 },
+    { title: "Sweater", img: img7 },
+    { title: "Shirt", img: img8 },
+    { title: "Scarf", img: img1 },
+    { title: "Short", img: img6 },
+
+  ]
+  return (
+
+
+    <div>
+      {
+        data.map((props) => {
+          return (
+            <MenSubCategoryCard title={props.title} imgsrc={props.img} />
+
+          )
+        }
+        )
+      }
+
+    </div>
   );
-  
+
 }
 export default MenSubCategoryCards;
