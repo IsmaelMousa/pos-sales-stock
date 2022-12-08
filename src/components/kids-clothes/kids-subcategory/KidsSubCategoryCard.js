9c3d14a8e54b71b1de8ffeb4de79f9d3d4071d99
unknown <renadiwidat2003@gmail.com>
2022-12-09 02:45:02 +0300

    edit the subcaregory file

--- src/components/kids-clothes/kids-subcategory/KidsSubCategoryCard.js
+++ src/components/kids-clothes/kids-subcategory/KidsSubCategoryCard.js
@@ -1,16 +1,22 @@
 import React from 'react';
-import './KidsSubCategoryCard.css'
+import { Link } from 'react-router-dom';
+import "./KidsSubCategoryCards.css"
 const KidsSubCategoryCard = (props) => {
-  return (         
-          <div className="card">
-            <img src={props.img} class="card-img-top" alt="..." />
-            <div class="card-body">
-            <h4 className='card-title'> {props.title}</h4>
-              <button type='button' className='btn btn-outline'> <b> Go</b></button>
-            </div>
-          </div>
-      
-     
+  return (
+    <div className="card">
+      <img src={props.img} class="card-img-top" alt="..." />
+      <div class="card-body">
+        <h4 className='card-title'> {props.title}</h4>
+        <button>
+          <Link to={"/kids/" + props.title}  ><b>Go</b></Link>
+
+        </button>
+
+
+      </div>
+    </div>
+
+
   );
 }
 export default KidsSubCategoryCard;
--- src/components/kids-clothes/kids-subcategory/KidsSubCategoryCards.js
+++ src/components/kids-clothes/kids-subcategory/KidsSubCategoryCards.js
@@ -7,30 +7,29 @@ import img5 from "../../../images/pant-kids-sub.webp";
 import img6 from "../../../images/short-kids-sub.webp";
 import img7 from "../../../images/dress-kids-sub.webp";
 import img8 from "../../../images/pyjama-kids-sub.webp";
-
-const KidsSubCategoryCards=()=> {
+const KidsSubCategoryCards = () => {
   const data = [
-    { title: " Hats", img: img1 },
+    { title: "Hat", img: img1 },
     { title: "Hoodies", img: img2 },
     { title: "Jackets", img: img3 },
     { title: "T-Shirts", img: img4 },
-    { title: "Pants ", img: img5 },
+    { title: "Pants", img: img5 },
     { title: "Shorts", img: img6 },
     { title: "Dresses", img: img7 },
     { title: "Pyjamas", img: img8 },
   ]
   return (
     <div>
-          {
-            data.map((item) => {
-              return (
-                <KidsSubCategoryCard title={item.title} img={item.img}/>
-              )
-            }
-            )
-          }
-        </div>
-    
+      {
+        data.map((item) => {
+          return (
+            <KidsSubCategoryCard title={item.title} img={item.img} />
+          )
+        }
+        )
+      }
+    </div>
+
 
   );
 }
