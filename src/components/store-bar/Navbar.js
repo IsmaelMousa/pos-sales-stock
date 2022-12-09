f563517e3c8573ea606b05aa449afbfe9b9d9d41
naseem <s12113453@stu.najah.edu>
2022-12-10 00:09:55 +0200

    store bar for seller page

--- src/components/store-bar/Navbar.js
+++ src/components/store-bar/Navbar.js
@@ -1,24 +1,48 @@
+import "./NavBar.css"
+import logo from "../../images/logo.webp";
+
 export default function Navbar() {
     const path = window.location.pathname
-    return <nav className="nav">
-        <a href="/" className="sits-title"> Logo</a>
+    return (
         <ul>
-            <li>
-                <a href="/kids"> Kids</a>
+            <li className="logo">
+            <a  className="img"  href="index.html"><img src={logo} class="img" alt="website"/></a>
             </li>
 
             <li>
-                <a href="/sale"> Sale</a>
+            <button className="category">
+            <a href="/sale"> Sale</a>
+            </button>
             </li>
-
-            <li>
-                <a href="/women"> Women</a>
-            </li>
-
-            <li>
+            
+            <li >
+            <button className="category">
                 <a href="/men"> Men</a>
+                </button>
+            </li>
+
+            <li>
+            <button className="category">
+                <a href="/women"> Women</a>
+                </button>
+            </li>
+
+            <li>
+            <button className="category">
+                <a href="/kids"> Kids</a>
+                </button>
+            </li>
+
+            <li className="return">
+                <button className="category">
+                    <a className="return" href="#">
+                        <i className="fa-solid fa-arrow-right-from-bracket" />
+                    </a>
+                </button>
             </li>
 
         </ul>
-    </nav>
+        
+    )
+    
 }
