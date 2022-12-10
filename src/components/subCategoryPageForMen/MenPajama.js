4661c5f322930fe471f3de4b93827f1b751a18a5
unknown <renadiwidat2003@gmail.com>
2022-12-10 11:35:00 +0300

    link SubCategories with thier items

---
+++ src/components/subCategoryPageForMen/MenPajama.js
@@ -0,0 +1,4 @@
+import MenPajamaPage from "../men-clothes/men-items/MenPajamaPage"
+export default function MenPajama() {
+    return (<MenPajamaPage />)
+}
---
+++ src/components/subCategoryPageForMen/MenShirt.js
@@ -0,0 +1,4 @@
+import MenShirtPage from "../men-clothes/men-items/MenShirtPage"
+export default function MenShirt() {
+    return (<MenShirtPage />)
+}
