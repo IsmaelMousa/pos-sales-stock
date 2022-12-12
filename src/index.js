c857ef288192072fdcaccc1d433d94a14582c6de
unknown <renadiwidat2003@gmail.com>
2022-12-12 22:41:29 +0300

    some modifiding like changing name of folders

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter, Route, Link, Routes } from "react-router-dom";
-import './App.css';
+
 import Logo from "./components/pages/logos"
 import Women from "./components/pages/Women"
 import Men from "./components/pages/Men"
@@ -42,41 +42,41 @@ root.render(
   <React.StrictMode>
     <BrowserRouter>
       <NavBar />
-  <div className="parent">
-    
-      <Routes>
-        <Route path="/" element={<Logo />} />
-        <Route path="/women" element={<Women />} />
-        <Route path="/men" element={<Men />} />
-        <Route path="/kids" element={<Kids />} />
-        <Route path="/sale" element={<Sale />} />
-        <Route path='/kids/Hat' element={<KidsHats />} />
-        <Route path='/kids/Hoodies' element={<KidsHoodies />} />
-        <Route path='/kids/T-Shirts' element={<KidsTShirt />} />
-        <Route path='/kids/Jackets' element={<KidsJackets />} />
-        <Route path='/kids/Pants' element={<KidsPants />} />
-        <Route path='/kids/Shorts' element={<KidsShorts />} />
-        <Route path='/kids/Dresses' element={<KidsDresses />} />
-        <Route path='/kids/Pyjamas' element={<KidsPajamas />} />
-        <Route path='/Women/Coats' element={<WomenCoats />} />
-        <Route path='/Women/Dresses' element={<WomenDressPage />} />
-        <Route path='/Women/Hoodies' element={<WomenHoodiePage />} />
-        <Route path='/Women/Jacket' element={<WomenJacket />} />
-        <Route path='/Women/Pants' element={<WomenPantPage />} />
-        <Route path='/Women/Pyjamas' element={<WomenPyjamas />} />
-        <Route path='/Women/Shirt' element={<WomenShirtsPage />} />
-        <Route path='/Women/T-Shirts' element={<WomenTShirt />} />
-        <Route path='/Men/Coat' element={<MenCoat />} />
-        <Route path='/Men/Jacket' element={<MenJacket />} />
-        <Route path='/Men/Pant' element={<MenPant />} />
-        <Route path='/Men/Shirt' element={<MenShirt />} />
-        <Route path='/Men/Short' element={<MenShort />} />
-        <Route path='/Men/Sweater' element={<MenSweater />} />
-        <Route path='/Men/T-Shirt' element={<MenTShirt />} />
-        <Route path='/Men/Pyjama' element={<MenPajama />} />
-      </Routes>
-      
-      <div><Table/></div>
+      <div className="parent">
+
+        <Routes>
+          <Route path="/" element={<Logo />} />
+          <Route path="/women" element={<Women />} />
+          <Route path="/men" element={<Men />} />
+          <Route path="/kids" element={<Kids />} />
+          <Route path="/sale" element={<Sale />} />
+          <Route path='/kids/Hat' element={<KidsHats />} />
+          <Route path='/kids/Hoodies' element={<KidsHoodies />} />
+          <Route path='/kids/T-Shirts' element={<KidsTShirt />} />
+          <Route path='/kids/Jackets' element={<KidsJackets />} />
+          <Route path='/kids/Pants' element={<KidsPants />} />
+          <Route path='/kids/Shorts' element={<KidsShorts />} />
+          <Route path='/kids/Dresses' element={<KidsDresses />} />
+          <Route path='/kids/Pyjamas' element={<KidsPajamas />} />
+          <Route path='/Women/Coats' element={<WomenCoats />} />
+          <Route path='/Women/Dresses' element={<WomenDressPage />} />
+          <Route path='/Women/Hoodies' element={<WomenHoodiePage />} />
+          <Route path='/Women/Jacket' element={<WomenJacket />} />
+          <Route path='/Women/Pants' element={<WomenPantPage />} />
+          <Route path='/Women/Pyjamas' element={<WomenPyjamas />} />
+          <Route path='/Women/Shirt' element={<WomenShirtsPage />} />
+          <Route path='/Women/T-Shirts' element={<WomenTShirt />} />
+          <Route path='/Men/Coat' element={<MenCoat />} />
+          <Route path='/Men/Jacket' element={<MenJacket />} />
+          <Route path='/Men/Pant' element={<MenPant />} />
+          <Route path='/Men/Shirt' element={<MenShirt />} />
+          <Route path='/Men/Short' element={<MenShort />} />
+          <Route path='/Men/Sweater' element={<MenSweater />} />
+          <Route path='/Men/T-Shirt' element={<MenTShirt />} />
+          <Route path='/Men/Pyjama' element={<MenPajama />} />
+        </Routes>
+
+        <div><Table /></div>
       </div>
     </BrowserRouter>
     <App />
