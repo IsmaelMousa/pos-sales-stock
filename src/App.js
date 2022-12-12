22edf1ad81a3f68d2fb98d1e354a473a31e8e3d4
borooj <boroojedaily@gmail.com>
2022-12-12 21:10:18 +0300

    edit styling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 function App() {
-  return (
-<div></div>
-  );
+  return <div></div>;
 }
 export default App;
--- src/components/shopping-cart/Table.js
+++ src/components/shopping-cart/Table.js
@@ -10,7 +10,7 @@ const Table = () => {
                     <th scope="col">Product Name</th>
                     <th scope="col">Quantity</th>
                     <th scope="col">Price</th>
-                    <th scope="col"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-trash-fill fa-3x" viewBox="0 0 16 16">
+                    <th scope="col" ><svg xmlns="http://www.w3.org/2000/svg" id="icon" width="25" height="25" fill="currentColor" className="bi bi-trash-fill fa-3x" viewBox="0 0 16 16">
                         <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z" />
                     </svg></th>
                 </tr>
@@ -20,7 +20,8 @@ const Table = () => {
                     <td>Jacket</td>
                     <td>1</td>
                     <td>30$</td>
-                    <td> <button type="button" class="btn btn-transparent"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"  className="bi bi-dash-circle" viewBox="0 0 16 16">
+                    <td> 
+                        <button  id ="icon1"><svg xmlns="http://www.w3.org/2000/svg"  width="25" height="25"  className="bi bi-dash-circle" viewBox="0 0 16 16">
   <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
   <path d="M4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8z"/>
 </svg></button></td>
@@ -29,8 +30,8 @@ const Table = () => {
                     <td>Hat</td>
                     <td>1</td>
                     <td>15$</td>
-                    <td> <button className="button">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-dash-circle" viewBox="0 0 16 16">
+                    <td> <button id ="icon1">
+                        <svg xmlns="http://www.w3.org/2000/svg"  width="25" height="25" fill="currentColor" className="bi bi-dash-circle" viewBox="0 0 16 16">
                             <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
                             <path d="M4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8z" />
                         </svg>
@@ -40,8 +41,8 @@ const Table = () => {
                     <td>dresses</td>
                     <td>1</td>
                     <td>68$</td>
-                    <td><button className="button">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-dash-circle" viewBox="0 0 16 16">
+                    <td><button id ="icon1">
+                        <svg xmlns="http://www.w3.org/2000/svg"  width="25" height="25" fill="currentColor" className="bi bi-dash-circle" viewBox="0 0 16 16">
                             <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
                             <path d="M4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8z" />
                         </svg>
@@ -53,9 +54,8 @@ const Table = () => {
                     <td>1</td>
                     <td>5$</td>
                     <td>
-                        <button className="button">
-
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-dash-circle" viewBox="0 0 16 16">
+                        <button id ="icon1">
+                            <svg xmlns="http://www.w3.org/2000/svg"  width="25" height="25" fill="currentColor" className="bi bi-dash-circle" viewBox="0 0 16 16">
                                 <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
                                 <path d="M4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8z" />
                             </svg>
@@ -63,9 +63,9 @@ const Table = () => {
                     </td>
                 </tr>
                 <tr>
-                    <td colSpan={3}>Total Price = 118$</td>
+                    <td colSpan={3}> <b>Total Price = 118$</b></td>
                     <td>
-                        <button className="button" onClick={Print}> Checkout</button></td>
+                        <button className="button1" onClick={Print}><b> Checkout </b></button></td>
                 </tr>
             </tbody>
         </table>
