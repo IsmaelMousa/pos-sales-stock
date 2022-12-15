ac5cf2786cbb991821a05a6f00519f1e98840d9a
naseem <s12113453@stu.najah.edu>
2022-12-16 00:28:27 +0200

    store bar for the admin page

---
+++ src/components/admin/Navbar/Navbar.js
@@ -0,0 +1,34 @@
+import "./Navbar.css"
+import logo from "../../../images/logo.webp";
+import { Link } from "react-router-dom";
+
+
+function NavBar() {
+  return (
+
+    <ul>
+
+   <li className='logo'>
+         <Link to="index.html">
+         <img src={logo} className="img" alt="website"/></Link>
+     </li>
+
+    <li className="button">
+         <Link to="/AdminProductsTable"></Link>
+         <i class="fa-solid fa-user-plus"></i>
+    </li>
+
+    <li  className="button">
+          <Link to="/AdminEmployeeTable"></Link>
+          <i class="fa-sharp fa-solid fa-cart-plus"></i>
+    </li>
+
+    <li  className='return'>
+         <Link to="#"></Link>
+         <i className="fa-solid fa-arrow-right-from-bracket"></i>
+    </li>
+
+  </ul>
+  )
+};
+export default NavBar;
