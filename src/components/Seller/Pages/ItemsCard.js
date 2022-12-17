56ce78931afbca1f7ec51058af2fce77e8237ce5
batool <s12115673@stu.najah.edu>
2022-12-17 22:37:29 +0200

    Modifay files

--- src/components/Seller/Pages/ItemsCard.js
+++ src/components/Seller/Pages/ItemsCard.js
@@ -3,7 +3,7 @@ const ItemsCard = (props) => {
   return (
     <div className="card ">
       <div className="overflow">
-        <img src={props.imgsrc} alt="pants" className="card-img-top" />
+        <img src={props.imgsrc} alt="..." className="card-img-top" />
       </div>
       <div className="card-body text-dark ">
         <h4 className="card-title"> {props.title}</h4>
--- src/components/Seller/Pages/SubCategoryCards.js
+++ src/components/Seller/Pages/SubCategoryCards.js
@@ -2,12 +2,9 @@ import SubCategoryCard from "./SubCategoryCard";
 import SubCategoriesData from "./SubCategoriesData";
 import { useEffect } from "react";
 import { useParams } from "react-router-dom";
-
-
 const SubCategoryCards = () => {
-  let  results = useParams();
+  let  {results} = useParams();
   const url = new URL(window.location.href);
-
   var data=SubCategoriesData.filter((item)=>item.category=== url.searchParams.get('category'));
   useEffect(()=>{
       data = SubCategoriesData.filter((item)=>item.category=== url.searchParams.get('category'))
