004c1795473d5b4cecc4c5beca267ee6d6db1247
unknown <renadiwidat2003@gmail.com>
2022-12-19 01:14:21 +0300

    linking sinin page with seller page

---
+++ src/components/Seller/Pages/Home1.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import '../../../index.css';
+import reportWebVitals from '../../../reportWebVitals';
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import SalesPage from "../../../components/Seller/Pages/SalesPage"
+import NavBar from "../../../components/Seller/NavBar/Navbar"
+import SubCategoryCards from "../../../components/Seller/Pages/SubCategoryCards"
+import Items from "../../../components/Seller/Pages/ItemsPage"
+import '../../../components/UI/SignInSignUp.css'
+
+function Home1() {
+
+    const root = ReactDOM.createRoot(document.getElementById('root'));
+    root.render(
+        <React.StrictMode>
+            <BrowserRouter>
+                <div>
+                    <NavBar />
+                    <div className="parent">
+                        <Routes>
+                            <Route path="/salePage" element={<SalesPage />} />
+                            <Route path="/subcategory" element={<SubCategoryCards />} />
+                            <Route path="/items" element={<Items />} />
+
+                        </Routes>
+                    </div>
+                </div>
+            </BrowserRouter>
+
+        </React.StrictMode>
+    );
+    reportWebVitals();
+
+}
+export default Home1;
