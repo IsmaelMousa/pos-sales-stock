6ca81b299fd34cb3738d1462683040e1ab7b1d4f
naseem <s12113453@stu.najah.edu>
2022-12-18 07:40:20 +0200

    style the sub category cards

--- src/components/Seller/Pages/SubCategoryCard.js
+++ src/components/Seller/Pages/SubCategoryCard.js
@@ -4,17 +4,21 @@ import { Link } from 'react-router-dom';
 
 const SubCategoryCard = (props) => {
     return (
+       
         <div className="card text-center">
-            <div className="overflow">
-                <img src={props.imgsrc} alt="..." className='card-img-top' />
+            <div className="overflow">   
+            <button className='ClickImage'>
+                <Link to={"/items?category="+props.category+"&subcategory=" + props.subcategory}   className='text3'> <img src={props.imgsrc} alt="..." className='card-img-top' /> </Link>
+                </button>
                 <div className='card-body '>
                     <h4 className='card-title'> {props.subcategory}</h4>
                     <button type='button' className="btn btn-outline" id="b3" >
-                        <Link to={"/items?category="+props.category+"&subcategory=" + props.subcategory}  ><b className='text3'> Go </b></Link>
+                        <Link to={"/items?category="+props.category+"&subcategory=" + props.subcategory}   className='text3'> Go </Link>
                     </button>
                 </div>
             </div>
         </div>
+       
     );
 }
 export default SubCategoryCard;
