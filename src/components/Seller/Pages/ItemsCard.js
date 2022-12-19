271ed428b0f735960a794346af06deed5c557a34
naseem <s12113453@stu.najah.edu>
2022-12-19 18:40:27 +0200

    modified on style the sub category cards

--- src/components/Seller/Pages/ItemsCard.js
+++ src/components/Seller/Pages/ItemsCard.js
@@ -1,15 +1,15 @@
 import "./ItemsCard.css";
 const ItemsCard = (props) => {
   return (
-    <div className="card text-center">
+    <div className="card text-center" id="card-item">
       <div className="overflow">
-        <img src={props.imgsrc} alt="pants" className="card-img-top" />
+        <img src={props.imgsrc} alt="pants" className="card-img-top" id="card-img-top-item"/>
       </div>
-      <div className="card-body text-dark ">
-        <h4 className="card-title"> {props.title}</h4>
-        <h5 className="price">{props.price}</h5>
-        <h6 className="quantity">RQ : {props.quantity}</h6>
-         <button type='button' className="take d-grid col-4 mx-auto" id="b3" >
+      <div className="card-body text-dark" id="card-body-item">
+        <h4 className="card-title" id="card-title-item"> {props.title}</h4>
+        <h5 className="price" id="price-item">{props.price}</h5>
+        <h6 className="quantity" id="quantity-item">RQ : {props.quantity}</h6>
+         <button type='button' className="d-grid col-4 mx-auto" id="b2" >
          Pick Up
         </button>
       </div>
