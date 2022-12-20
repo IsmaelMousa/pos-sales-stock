21c73c49ecca7df17a590330f8ca64273f51908a
Ismael Mousa <ismaelramzimousa@gmail.com>
2022-12-20 23:27:29 +0200

    add css for cart table

--- src/components/Seller/Cart/Cart.js
+++ src/components/Seller/Cart/Cart.js
@@ -16,39 +16,39 @@ function Cart({ products, empty, remove }) {
   };
 
   return (
-    <div className>
-      <div className>
-        <div className>
-          <table>
+    <div className="cart">
+      <div className="cart-info">
+        <div className="cart-table">
+          <table className="table">
             <thead>
               <tr>
-                <th className>Name</th>
-                <th className>Qty</th>
-                <th className>Price</th>
-                <th className>Action</th>
+                <th className="items-name">Name</th>
+                <th className="items-qty">Qty</th>
+                <th className="items-price">Price</th>
+                <th className="remove-icon">Action</th>
               </tr>
             </thead>
             <tbody>
               {products?.map((item, index) => (
                 <tr key={index}>
-                  <td className>{item.name}</td>
-                  <td>1</td>
-                  <td>{item.price}</td>
-                  <td onClick={() => DeleteItem(item)} className>❌</td>
+                  <td className="items-name">{item.name}</td>
+                  <td className="items-qty">1</td>
+                  <td className="items-price">{item.price}</td>
+                  <td onClick={() => DeleteItem(item)} className="remove-icon">❌</td>
                 </tr>
               ))}
             </tbody>
           </table>
         </div>
-        <div className>
+        <div className="total">
           <hr />
-          <div className>Total:{total}$</div>
+          <div className="total-price">Total:{total}$</div>
           <hr />
         </div>
       </div>
-      <div className>
-        <button onClick={() => PrintCart()}>Pay</button>
-        <button onClick={() => DeleteCart()}>Cancel</button>
+      <div className="cart-actions">
+        <button onClick={() => PrintCart()}className="btn btn-outline-dark">Pay</button>
+        <button onClick={() => DeleteCart()}className="btn btn-outline-danger">Cancel</button>
       </div>
     </div>
   );
