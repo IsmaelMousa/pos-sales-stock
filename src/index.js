9cbf122ba6873799c277220b6da51e04b6e9397f
unknown <renadiwidat2003@gmail.com>
2022-12-21 15:28:06 +0300

    deleting admin pages and modified app.js file

--- src/index.js
+++ src/index.js
@@ -9,18 +9,15 @@ import SubCategoryCards from "./components/Seller/Products/SubCategoryCards"
 import Search from "./components/Seller/Products/Search"
 import Items from "./components/Seller/Products/ItemsPage"
 import NavBar from "./components/Seller/NavBar/Navbar"
-import Table from "./components/Admin/Employee/Table"
-import AdminProductsTable from "./components/Admin/Products/Table"
-import AdminNavBar from "./components/Admin/NavBar/AdminNavBar"
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<SignInSignupWithLocalStorage />} />
-        <Route path="/admin" element={<><AdminNavBar /><AdminProductsTable/></>} />
-        <Route path='/employee' element={<><AdminNavBar/><Table /></>} />
-        <Route path='/product' element={<> <AdminNavBar/><AdminProductsTable /></>} />
+        <Route path="/admin" element={<></>} />
+        {/* <Route path='/employee' element={<><AdminNavBar/><Table /></>} />
+        <Route path='/product' element={<> <AdminNavBar/><AdminProductsTable /></>} /> */}
         <Route path="/seller" element={<><NavBar /><div className="parent"><SalesPage /></div></>} />
         <Route path="/subcategory/:category" element={<><NavBar /><div className="parent"><SubCategoryCards /></div></>} />
         <Route path="/items/:category/:subcategory" element={<><NavBar /><div className="parent"><Items /></div></>} />
