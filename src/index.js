766bc74daa885a7f3591f479009d052bd63b6f7e
Ismael Mousa <ismaelramzimousa@gmail.com>
2022-12-21 21:22:05 +0200

    add admin page

--- src/index.js
+++ src/index.js
@@ -9,13 +9,14 @@ import SubCategoryCards from "./components/Seller/Products/SubCategoryCards"
 import Search from "./components/Seller/Products/Search"
 import Items from "./components/Seller/Products/ItemsPage"
 import NavBar from "./components/Seller/NavBar/Navbar"
+import Table from './components/Admin/Table';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<SignInSignupWithLocalStorage />} />
-        <Route path="/admin" element={<></>} />
+        <Route path="/admin" element={<div className='container'><Table/></div>} />
         {/* <Route path='/employee' element={<><AdminNavBar/><Table /></>} />
         <Route path='/product' element={<> <AdminNavBar/><AdminProductsTable /></>} /> */}
         <Route path="/seller" element={<><NavBar /><div className="parent"><SalesPage /></div></>} />
